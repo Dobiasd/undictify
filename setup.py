d6d5247d716f9ac7a2bee18592c9fc9cc9a3c1b7
Dobiasd <editgym@gmail.com>
2018-08-05 21:47:53 +0200

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="undictify",
-    version="0.2.0",
+    version="0.2.1",
     author="Tobias Hermann",
     author_email="editgym@gmail.com",
     description="Type-checked function calls at runtime",
