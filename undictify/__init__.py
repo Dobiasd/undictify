352036ab9d0cb649b82e33cfd9396066cd6ea05a
Dobiasd <editgym@gmail.com>
2018-08-05 09:37:34 +0200

    Move test into separate file

--- undictify/__init__.py
+++ undictify/__init__.py
@@ -1,3 +1,8 @@
 from ._unpack import unpack_dict, unpack_json
 
 name = "undictify"
+
+__author__ = "Tobias Hermann"
+__copyright__ = "Copyright 2018, Tobias Hermann"
+__email__ = "editgym@gmail.com"
+__license__ = "MIT"
