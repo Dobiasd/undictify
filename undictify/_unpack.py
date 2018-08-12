1e0a97f81eb61b95d8f8a60ac71f3b972359a263
Dobiasd <editgym@gmail.com>
2018-08-12 09:02:47 +0200

    Use leading single underscores as naming convention for internal functions

--- undictify/_unpack.py
+++ undictify/_unpack.py
@@ -32,7 +32,7 @@ def type_checked_call(skip: bool = False,
                                 _merge_args_and_kwargs(func, *args, **kwargs),
                                 skip, convert)
 
-        setattr(wrapper, '_undictify_wrapped_func', func)
+        setattr(wrapper, '__undictify_wrapped_func__', func)
         return wrapper
 
     return call_decorator
@@ -42,7 +42,7 @@ WrappedOrFunc = Callable[..., TypeT]
 
 
 def _is_wrapped_func(func: WrappedOrFunc) -> bool:
-    return hasattr(func, '_undictify_wrapped_func')
+    return hasattr(func, '__undictify_wrapped_func__')
 
 
 def _merge_args_and_kwargs(func: Callable[..., TypeT],
@@ -70,9 +70,9 @@ def _unpack_dict(func: WrappedOrFunc,
                  convert_types: bool) -> Any:
     """Constructs an object in a type-safe way from a dictionary."""
 
-    assert __is_dict(data), 'Argument data needs to be a dictionary.'
+    assert _is_dict(data), 'Argument data needs to be a dictionary.'
 
-    signature = inspect.signature(__unwrap_decorator_type(func))
+    signature = inspect.signature(_unwrap_decorator_type(func))
     ctor_params: Dict[str, Any] = {}
 
     if not skip_superfluous:
@@ -86,39 +86,39 @@ def _unpack_dict(func: WrappedOrFunc,
         if param.kind != inspect.Parameter.POSITIONAL_OR_KEYWORD:
             raise TypeError('Only parameters of kind POSITIONAL_OR_KEYWORD '
                             'supported in target functions.')
-        if __is_union_type(param.annotation) \
-                and not __is_optional_type(param.annotation):
+        if _is_union_type(param.annotation) \
+                and not _is_optional_type(param.annotation):
             raise TypeError('Union members in target function other than Optional '
                             'are not supported.')
-        if __is_dict_type(param.annotation):
+        if _is_dict_type(param.annotation):
             raise TypeError('Dict members in target function are not supported.')
         if param.name not in data:
-            if __is_optional_type(param.annotation):
+            if _is_optional_type(param.annotation):
                 ctor_params[param.name] = None
             else:
                 raise TypeError(f'Key {param.name} is missing.')
         else:
-            ctor_params[param.name] = __get_value(param.annotation,
-                                                  data[param.name],
-                                                  param.name,
-                                                  skip_superfluous,
-                                                  convert_types)
+            ctor_params[param.name] = _get_value(param.annotation,
+                                                 data[param.name],
+                                                 param.name,
+                                                 skip_superfluous,
+                                                 convert_types)
 
-    return __unwrap_decorator_type(func)(**ctor_params)
+    return _unwrap_decorator_type(func)(**ctor_params)
 
 
-def __get_value(func: WrappedOrFunc, value: Any, log_name: str,
-                skip_superfluous: bool, convert_types: bool) -> Any:
+def _get_value(func: WrappedOrFunc, value: Any, log_name: str,
+               skip_superfluous: bool, convert_types: bool) -> Any:
     """Convert a single value into target type if possible."""
-    if __is_list(value):
-        return __get_list_value(func, value, log_name,
-                                skip_superfluous, convert_types)
+    if _is_list(value):
+        return _get_list_value(func, value, log_name,
+                               skip_superfluous, convert_types)
 
-    if __is_dict(value):
-        return __get_dict_value(func, value)  # Use settings of inner value
+    if _is_dict(value):
+        return _get_dict_value(func, value)  # Use settings of inner value
 
-    allowed_types = list(map(__unwrap_decorator_type, __get_union_types(func) \
-        if __is_optional_type(func) \
+    allowed_types = list(map(_unwrap_decorator_type, _get_union_types(func) \
+        if _is_optional_type(func) \
         else [func]))
 
     if func is inspect.Parameter.empty and log_name != 'self':
@@ -126,164 +126,164 @@ def __get_value(func: WrappedOrFunc, value: Any, log_name: str,
                         'is missing a type annotation.')
 
     if Any not in allowed_types and log_name != 'self':
-        if not __isinstanceofone(value, allowed_types):
+        if not _isinstanceofone(value, allowed_types):
             value_type = type(value)
             if convert_types:
-                if __is_optional_type(func):
-                    func = __get_optional_type(func)
+                if _is_optional_type(func):
+                    func = _get_optional_type(func)
                 try:
                     return func(value)
                 except ValueError:
                     raise TypeError(f'Can not convert {value} '
-                                    f'from type {__get_type_name(value_type)} '
-                                    f'into type {__get_type_name(func)} '
+                                    f'from type {_get_type_name(value_type)} '
+                                    f'into type {_get_type_name(func)} '
                                     f'for key {log_name}.')
 
             raise TypeError(f'Key {log_name} has incorrect type: '
-                            f'{__get_type_name(value_type)} instead of '
-                            f'{__get_type_name(func)}.')
+                            f'{_get_type_name(value_type)} instead of '
+                            f'{_get_type_name(func)}.')
 
     return value
 
 
-def __get_list_value(func: Callable[..., TypeT], value: Any,
-                     log_name: str,
-                     skip_superfluous: bool, convert_types: bool) -> Any:
-    if not __is_list_type(func) and \
-            not __is_optional_list_type(func):
+def _get_list_value(func: Callable[..., TypeT], value: Any,
+                    log_name: str,
+                    skip_superfluous: bool, convert_types: bool) -> Any:
+    if not _is_list_type(func) and \
+            not _is_optional_list_type(func):
         raise TypeError(f'No list expected for {log_name}')
     result = []
-    result_elem_type = __get_list_type_elem_type(func)
+    result_elem_type = _get_list_type_elem_type(func)
     for elem in value:
-        result.append(__get_value(result_elem_type,
-                                  elem, value,
-                                  skip_superfluous, convert_types))
+        result.append(_get_value(result_elem_type,
+                                 elem, value,
+                                 skip_superfluous, convert_types))
     return result
 
 
-def __get_dict_value(func: Callable[..., TypeT], value: Any) -> Any:
-    assert __is_dict(value)
-    if __is_optional_type(func):
-        return __get_optional_type(func)(**value)  # type: ignore
+def _get_dict_value(func: Callable[..., TypeT], value: Any) -> Any:
+    assert _is_dict(value)
+    if _is_optional_type(func):
+        return _get_optional_type(func)(**value)  # type: ignore
     return func(**value)
 
 
-def __is_union_type(the_type: Callable[..., TypeT]) -> bool:
+def _is_union_type(the_type: Callable[..., TypeT]) -> bool:
     """Return True if the type is a Union."""
     if VER_3_7_AND_UP:
         return (the_type is Union or
-                __is_instance(the_type, _GenericAlias) and __type_origin_is(the_type, Union))
-    return __is_instance(the_type, _Union)
+                _is_instance(the_type, _GenericAlias) and _type_origin_is(the_type, Union))
+    return _is_instance(the_type, _Union)
 
 
-def __is_list_type(the_type: Callable[..., TypeT]) -> bool:
+def _is_list_type(the_type: Callable[..., TypeT]) -> bool:
     """Return True if the type is a List."""
     try:
         if VER_3_7_AND_UP:
-            return __is_instance(the_type,
-                                 _GenericAlias) and __type_origin_is(the_type, list)
+            return _is_instance(the_type,
+                                _GenericAlias) and _type_origin_is(the_type, list)
         return issubclass(the_type, List)  # type: ignore
     except TypeError:
         return False
 
 
-def __is_optional_list_type(the_type: Callable[..., TypeT]) -> bool:
+def _is_optional_list_type(the_type: Callable[..., TypeT]) -> bool:
     """Return True if the type is a Optional[List]."""
-    if __is_list_type(the_type):
+    if _is_list_type(the_type):
         return True
-    if __is_optional_type(the_type) and \
-            __is_list_type(__get_optional_type(the_type)):
+    if _is_optional_type(the_type) and \
+            _is_list_type(_get_optional_type(the_type)):
         return True
     return False
 
 
-def __is_dict_type(the_type: Callable[..., TypeT]) -> bool:
+def _is_dict_type(the_type: Callable[..., TypeT]) -> bool:
     """Return True if the type is a Dict."""
     try:
         if VER_3_7_AND_UP:
-            return __is_instance(the_type,
-                                 _GenericAlias) and __type_origin_is(the_type, dict)
+            return _is_instance(the_type,
+                                _GenericAlias) and _type_origin_is(the_type, dict)
         return issubclass(the_type, Dict)  # type: ignore
     except TypeError:
         return False
 
 
-def __type_origin_is(the_type: Callable[..., TypeT], origin: Any) -> bool:
+def _type_origin_is(the_type: Callable[..., TypeT], origin: Any) -> bool:
     assert hasattr(the_type, '__origin__')
     return the_type.__origin__ is origin  # type: ignore
 
 
-def __get_union_types(union_type: Callable[..., TypeT]) -> List[Callable[..., TypeT]]:
+def _get_union_types(union_type: Callable[..., TypeT]) -> List[Callable[..., TypeT]]:
     """Return all types a Union can hold."""
-    assert __is_union_type(union_type)
+    assert _is_union_type(union_type)
     return union_type.__args__  # type: ignore
 
 
-def __get_optional_type(optional_type: Callable[..., TypeT]) -> Type[TypeT]:
+def _get_optional_type(optional_type: Callable[..., TypeT]) -> Type[TypeT]:
     """Return the type an Optional can hold."""
-    assert __is_optional_type(optional_type)
+    assert _is_optional_type(optional_type)
     args = optional_type.__args__  # type: ignore
     assert len(args) == 2
     return args[0]  # type: ignore
 
 
-def __get_type_name(the_type: Callable[..., TypeT]) -> str:
+def _get_type_name(the_type: Callable[..., TypeT]) -> str:
     """Return a printable name of a type."""
-    if __is_optional_type(the_type):
-        return f'Optional[{str(__get_optional_type(the_type).__name__)}]'
-    if __is_list_type(the_type):
-        return f'List[{str(__get_list_type_elem_type(the_type).__name__)}]'
+    if _is_optional_type(the_type):
+        return f'Optional[{str(_get_optional_type(the_type).__name__)}]'
+    if _is_list_type(the_type):
+        return f'List[{str(_get_list_type_elem_type(the_type).__name__)}]'
     return the_type.__name__
 
 
-def __get_list_type_elem_type(list_type: Callable[..., TypeT]) -> Callable[..., Any]:
+def _get_list_type_elem_type(list_type: Callable[..., TypeT]) -> Callable[..., Any]:
     """Return the type of a single element of the list type."""
-    if __is_optional_type(list_type):
-        list_type = __get_optional_type(list_type)
-    assert __is_list_type(list_type)
+    if _is_optional_type(list_type):
+        list_type = _get_optional_type(list_type)
+    assert _is_list_type(list_type)
     list_args = list_type.__args__  # type: ignore
     assert len(list_args) == 1
     return list_args[0]  # type: ignore
 
 
-def __isinstanceofone(value: Callable[..., TypeT], types: List[Callable[..., TypeT]]) -> bool:
+def _isinstanceofone(value: Callable[..., TypeT], types: List[Callable[..., TypeT]]) -> bool:
     """Check if value is an instance of one of the given types."""
     for the_type in types:
-        if __is_union_type(the_type):
-            if __isinstanceofone(value, __get_union_types(the_type)):
+        if _is_union_type(the_type):
+            if _isinstanceofone(value, _get_union_types(the_type)):
                 return True
         try:
-            if __is_instance(value, the_type):  # type: ignore
+            if _is_instance(value, the_type):  # type: ignore
                 return True
         except TypeError:
             pass
     return False
 
 
-def __is_optional_type(the_type: Callable[..., TypeT]) -> bool:
+def _is_optional_type(the_type: Callable[..., TypeT]) -> bool:
     """Return True if the type is an Optional."""
-    if not __is_union_type(the_type):
+    if not _is_union_type(the_type):
         return False
-    union_args = __get_union_types(the_type)
-    return len(union_args) == 2 and __is_instance(None, union_args[1])
+    union_args = _get_union_types(the_type)
+    return len(union_args) == 2 and _is_instance(None, union_args[1])
 
 
-def __is_dict(value: TypeT) -> bool:
+def _is_dict(value: TypeT) -> bool:
     """Return True if the value is a dictionary."""
     return isinstance(value, dict)
 
 
-def __is_list(value: TypeT) -> bool:
+def _is_list(value: TypeT) -> bool:
     """Return True if the value is a list."""
     return isinstance(value, list)
 
 
-def __unwrap_decorator_type(func: WrappedOrFunc) -> Callable[..., Any]:
+def _unwrap_decorator_type(func: WrappedOrFunc) -> Callable[..., Any]:
     """Get the actual type returned by the internal wrapper"""
     if _is_wrapped_func(func):
-        return getattr(func, '_undictify_wrapped_func')  # type: ignore
+        return getattr(func, '__undictify_wrapped_func__')  # type: ignore
     return func
 
 
-def __is_instance(value: TypeT, the_type: Callable[..., TypeT]) -> bool:
+def _is_instance(value: TypeT, the_type: Callable[..., TypeT]) -> bool:
     return isinstance(value, the_type)  # type: ignore
