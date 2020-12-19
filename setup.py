f7f82d2e72e5aa8d8ec89d164c38aaef74230b7b
Dobiasd <editgym@gmail.com>
2020-12-19 08:13:10 +0100

    Bump version number to 0.10.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="undictify",
-    version="0.9.0",
+    version="0.10.0",
     author="Tobias Hermann",
     author_email="editgym@gmail.com",
     description="Type-checked function calls at runtime",
