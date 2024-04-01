d52a1a1950bbb3fff6e8b1c3a4f7d0bb7c255c89
Dobiasd <editgym@gmail.com>
2024-04-01 21:29:24 +0200

    Fix empty unit-test error code with Python 3.12

--- test/run_code_checks.sh
+++ test/run_code_checks.sh
@@ -3,4 +3,4 @@ set -e
 
 find undictify -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint
 find undictify -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict
-find undictify -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 python3 -m unittest
+python3 -m unittest undictify/tests.py
